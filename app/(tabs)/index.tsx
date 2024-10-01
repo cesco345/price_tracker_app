31036362095267cd29f45b7c090fe0f9d0a2635b
Francesco Piscani <fpiscani@gmail.com>
2024-09-30 22:19:57 -0400

    add init and search box

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,32 @@
 import { Stack } from 'expo-router';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from 'react';
+import { Pressable, View, TextInput, Text } from 'react-native';
 
 import { ScreenContent } from '~/components/ScreenContent';
 
 export default function Home() {
+  const [search, setSearch] = useState('123152');
+  const performSearch = () => {
+    console.warn('searching: ', search);
+    // save this search to the database
+
+    // navigate to the search results after saving the search
+  };
+
   return (
     <>
-      <Stack.Screen options={{ title: 'Tab One' }} />
-      <View style={styles.container}>
-        <ScreenContent path="app/(tabs)/index.tsx" title="Tab One" />
+      <Stack.Screen options={{ title: 'Search' }} />
+      <View className="flex-row gap-3 p-3 ">
+        <TextInput
+          value={search}
+          onChangeText={setSearch}
+          placeholder="Search for a product"
+          className="flex-1 rounded border border-gray-300 bg-white p-3"
+        />
+        <Pressable onPress={performSearch} className="rounded bg-teal-500 p-3">
+          <Text>Search</Text>
+        </Pressable>
       </View>
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 24,
-  },
-});
